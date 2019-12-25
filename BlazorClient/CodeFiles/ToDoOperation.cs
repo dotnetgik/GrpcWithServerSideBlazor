ed14e83f82f8004c4f0a17f85fe6fb2323f3613e
Mangesh_Gaherwar <Mangesh.g@outlook.com>
2019-12-25 20:52:38 +0530

    Added Changes for the Client and Server

---
+++ BlazorClient/CodeFiles/ToDoOperation.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorClient.CodeFiles
+{
+    public partial class ToDoOperation : ComponentBase
+    {
+        public bool ShowModel = false;
+        public bool ShowAlert = false;
+        public bool ShowModeletePopup = false;
+        public string OperationStatusText = "";
+        public string PopupTitle = "";
+
+        public BlazorClient.Data.ToDoDataItem ToDoDataItem = null;
+        public string ActionText = "";
+
+        public ToDoGrpcService.ToDoItems toDoItems;
+
+        public string DeleteItemId { get; set; }
+
+        [Inject]
+        protected BlazorClient.Services.ToDoDataService ToDoService { get; set; } 
+
+
+        protected override void OnInitialized()
+        {
+
+            GetToDoList();
+        }
+
+        protected void GetToDoList()
+        {
+
+            toDoItems= ToDoService.GetToDoList();
+        }
+
+        protected async Task ShowEditForm(int Id)
+        {
+            
+            PopupTitle = "To Do Edit";
+            ActionText = "Update";
+
+            ToDoDataItem = ToDoService.GetToDoItem(Id);
+            ShowModel = true;
+        }
+
+        protected void ShowAddpopup()
+        {
+            ToDoDataItem = new Data.ToDoDataItem() { Title = "", Description = "", Status = false, Id = 0 };
+            PopupTitle = "To Do Add";
+            ActionText = "Add";
+            ShowModel = true;
+        }
+        protected void ShowDeletePopup(string Id)
+        {
+            DeleteItemId = Id;
+            ShowModeletePopup = true;
+        }
+
+
+
+        protected void PostData()
+        {
+            bool status = false;
+            if (ToDoDataItem.Id > 0)
+            {
+                status = ToDoService.UpdateToDoData(this.ToDoDataItem);
+
+            }
+            else
+            {
+                status = ToDoService.AddToDoData(this.ToDoDataItem);
+            }
+            Reload(status);
+        }
+
+        public void DeleteData()
+        {
+            
+            var operationStatus = ToDoService.DeleteData(DeleteItemId);
+            Reload(operationStatus);
+        }
+
+        protected void Reload(bool status)
+        {
+            ShowModeletePopup = false;
+            ShowModel = false;
+            GetToDoList();
+            ShowAlert = true;
+            if (status)
+            {
+                OperationStatusText = "Processed Successfully !! ";
+            }
+            else
+            {
+                OperationStatusText = "Error Occured  ";
+            }
+
+        }
+
+        protected void DismissPopup()
+        {
+            ShowModel = false;
+            ShowAlert = false;
+            ShowModeletePopup = false;
+        }
+
+    }
+}
---
+++ BlazorClient/Services/ToDoDataService.cs
@@ -0,0 +1,72 @@
+﻿using Grpc.Net.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorClient.Services
+{
+    public class ToDoDataService
+    {
+
+
+        private ToDoGrpcService.ToDoService.ToDoServiceClient GetServiceClient()
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            return new ToDoGrpcService.ToDoService.ToDoServiceClient(channel);
+        }
+
+        public bool AddToDoData(Data.ToDoDataItem toDoDataItem)
+        {
+
+            var client = GetServiceClient();
+
+            var todoData = new ToDoGrpcService.ToDoData()
+            {
+                Status = toDoDataItem.Status,
+                Title = toDoDataItem.Title,
+                Description = toDoDataItem.Description
+            };
+
+            var response = client.PostToDoItem(todoData, null);
+            return response.Status;
+
+        }
+        public bool UpdateToDoData(Data.ToDoDataItem toDoDataItem)
+        {
+            var client = GetServiceClient();
+            var updateData = new ToDoGrpcService.ToDoPutQuery();
+            updateData.Id = toDoDataItem.Id;
+            updateData.ToDoDataItem = new ToDoGrpcService.ToDoData()
+            {
+                Id = toDoDataItem.Id,
+                Status = toDoDataItem.Status,
+                Title = toDoDataItem.Title,
+                Description = toDoDataItem.Description
+            };
+            var response = client.PutToDoItem(updateData, null);
+            return response.Status;
+        }
+        public bool DeleteData(string ToDoId)
+        {
+            var client = GetServiceClient();
+            var response = client.DeleteItem(new ToDoGrpcService.ToDoQuery() { Id = Convert.ToInt32(ToDoId) }, null);
+            return response.Status;
+        }
+        public ToDoGrpcService.ToDoItems GetToDoList()
+        {
+            var client = GetServiceClient();
+            return  client.GetToDo(new Google.Protobuf.WellKnownTypes.Empty(), null);
+            
+        }
+
+        public Data.ToDoDataItem GetToDoItem(int id)
+        {
+            var client = GetServiceClient();
+            var todoItem = client.GetToDoItem(new ToDoGrpcService.ToDoQuery() { Id = Convert.ToInt32(id) }, null);
+
+            return new Data.ToDoDataItem() { Title = todoItem.Title, Description = todoItem.Description, Status = todoItem.Status, Id = todoItem.Id };
+
+        }
+    }
+}
